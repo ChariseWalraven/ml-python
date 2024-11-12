adb3f278a8aaa80d65af9168f6bde98ff8acafe8
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2024-11-12 18:49:48 +0100

    uses libraries in poland dataset; plots a couple of feats

--- linear_regression.py
+++ linear_regression.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/Titanic-Dataset.csv')
+df = pd.read_csv('data/libraries_in_Poland.csv', sep=";")
 
 # TODO: plot
-cols = ['Age', 'Fare']
-age_v_survived = df[cols]
+cols = ['Number_of_libraries', 'Number_of_librarians']
+f1_f2 = df[cols]
 
-age_v_survived.plot(kind="scatter", x=cols[0], y=cols[1])
+f1_f2.plot(kind="scatter", x=cols[0], y=cols[1])
 
-print(df)
-
-plt.show()
 
 # TODO: split into test and train groups, keeping only the features you want
+# print(df, f1_f2)
+plt.show()
 
