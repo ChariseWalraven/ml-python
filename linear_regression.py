7bf00885bf29f5f19ec42a6914b380b63d49b530
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2024-11-13 20:29:34 +0100

    completes linear regression using some library data

--- linear_regression.py
+++ linear_regression.py
@@ -1,16 +1,54 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+def plot_columns(columns):
+    """Plots the given 2 columns on a scatter plot."""
+    f1_f2 = df[columns]
+    f1_f2.plot(kind="scatter", x=columns[0], y=columns[1])
+    plt.show()
 
 df = pd.read_csv('data/libraries_in_Poland.csv', sep=";")
 
-# TODO: plot
+# Select features to use for input/prediction
 cols = ['Number_of_libraries', 'Number_of_librarians']
-f1_f2 = df[cols]
-
-f1_f2.plot(kind="scatter", x=cols[0], y=cols[1])
 
 
-# TODO: split into test and train groups, keeping only the features you want
-# print(df, f1_f2)
+# get input (x) and output (y)
+libraries_x = df[[cols[0]]]
+libraries_y = df[[cols[1]]]
+
+# NOTE: I get an error relating to passing a pandas Series instead of a DataFrame with a single row or column.
+#       When I google this, I get the recommendation to use the following syntax: df.iloc[:,0:1], but am not
+#       sure why or what the difference is
+
+# split into train and test
+x_train = libraries_x[0:-3]
+y_train = libraries_y[0:-3]
+
+x_test = libraries_x[-3:]
+y_test = libraries_y[-3:]
+
+regression_model = linear_model.LinearRegression()
+
+regression_model.fit(x_train, y_train)
+
+libraries_y_predictions = regression_model.predict(x_test)
+
+print("Coefficients: \n", regression_model.coef_)
+# The mean squared error
+print("Mean squared error: %.2f" % mean_squared_error(y_test, libraries_y_predictions))
+# The coefficient of determination: 1 is perfect prediction
+print("Coefficient of determination (r2 score): %.2f" % r2_score(y_test, libraries_y_predictions))
+
+plt.scatter(x_test, y_test, color="black")
+plt.scatter(x_train, y_train, color="blue")
+plt.plot(x_test, libraries_y_predictions, color="blue", linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
 plt.show()
 
+# plot_columns(cols)
