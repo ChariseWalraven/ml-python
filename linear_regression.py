5d8f38e846c50d2fdaaa1f87fbda5b79cb1e292a
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2024-11-19 15:09:37 +0100

    implements multivariate linear regression; gets confused about output; asks Merijn; turns out it's working fine, I just misunderstood what the outputs mean and when they're relevant.

--- linear_regression.py
+++ linear_regression.py
@@ -2,40 +2,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv('data/libraries_in_Poland.csv', sep=";")
 
 # Select features to use for input/prediction
 x_cols = ['Number_of_libraries', 'Amount_of_books', 'Number_of_loans', 'Number_of_librarians',
+# select features to use for input/prediction
+x_cols = ['Number_of_libraries', 'Amount_of_books', 'Number_of_loans', 'Number_of_readers',
           'Number_with_accessibility_features',
           'Number_with_accessible_entrance',
           'Number_with_accessible_interior', ]
-y_col = 'Number_of_readers'
+y_col = 'Number_of_librarians'
+
+# get input (X) and output (y)
+X = df[x_cols]
+y = df[[y_col]]
 
 # get input (x) and output (y)
 libraries_x = df[x_cols]
 libraries_y = df[[y_col]]
 
-# split into train and test
-x_train = libraries_x[0:-3]
-y_train = libraries_y[0:-3]
+# creating train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
 
-x_test = libraries_x[-3:]
-y_test = libraries_y[-3:]
+regression_model = linear_model.LinearRegression()
 
-# regression_model = linear_model.LinearRegression()
+regression_model.fit(X_train, y_train)
 
-# regression_model.fit(x_train, y_train)
+libraries_y_predictions = regression_model.predict(X_test)
 
-# libraries_y_predictions = regression_model.predict(x_test)
-#
-# # ?
-# print("Coefficients: \n", regression_model.coef_)
-# # how wrong the results are
-# print("Mean squared error: %.2f" % mean_squared_error(y_test, libraries_y_predictions))
-# # how accurate the predictions are
-# print("Coefficient of determination (r2 score): %.2f" % r2_score(y_test, libraries_y_predictions))
+# coefficients are the m's in the mx + b formula. They tell us what features are more important,
+# given the absolute size of the coefficients.
+print("Coefficients: \n", regression_model.coef_)
+# how wrong the results are
+print("Mean squared error: %.2f" % mean_squared_error(y_test, libraries_y_predictions))
+print("Mean absolute error: %.2f" % mean_absolute_error(y_test, libraries_y_predictions))
+# how accurate the predictions are
+print("Coefficient of determination (r2 score): %.2f" % r2_score(y_test, libraries_y_predictions))
 
 
 def plot_features():
